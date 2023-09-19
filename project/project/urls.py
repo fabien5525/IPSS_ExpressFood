2cf9b8616ac07c3c7117cd824a546a93b687a1e2
Tristan <tristan.chauvin.28@gmail.com>
2023-09-19 09:52:44 +0200

    Liste (getAll) + update plat du jour

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,23 +1,31 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from personne_api.views import PersonneViewSet
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
-router = routers.DefaultRouter()
-router.register(r'personnes', PersonneViewSet)
+from api import views
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="ExpressFood API",
-        default_version='v1',
-    ),
-    public=True,
-)
+# router = routers.DefaultRouter()
+# router.register(r'personnes', PersonneViewSet)
+
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="ExpressFood API",
+#         default_version='v1',
+#     ),
+#     public=True,
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('api/', include('api.urls')),
+    path('ListePlat/', views.getAllPlat, name='ListePlat'),
+    path('ListeUser/', views.getAllUser, name='ListeUser'),
+    path('ListeLivreur/', views.getAllLivreur, name='ListeLivreur'),
+    path('ListeCommande/', views.getAllCommande, name='ListeCommande'),
+    path('EditerPlatduJour/<int:id>', views.updatePlatduJour, name='EditerPlatduJour'),
+    # path('api/', include(router.urls)),
+    # path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
 ]
